8762a75e2d68b4a22a28d6c329048048e2e56771
Flowmikro <cloplop091@gmail.com>
2023-05-17 19:35:05 +0300

    v1.1

--- store/tests.py
+++ store/tests.py
@@ -1,3 +1,17 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+from .serializers import BookSerializers
+from store.models import Book
 
-# Create your tests here.
+
+class BookAPITestCase(APITestCase):
+    def test_get(self):
+        book_1 = Book.objects.create(name='test', price=25)
+        book_2 = Book.objects.create(name='test1', price=125)
+        url = reverse('book-list')
+        response = self.client.get(url)
+        serializers_data = BookSerializers([book_1, book_2], many=True).data
+        self.assertEqual(status.HTTP_200_OK, response.status_code)
+        self.assertEqual(serializers_data, response.data)
+
