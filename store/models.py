23fca34a09bbd6092651d3b332d9377f2aaae6ec
Flowmikro <cloplop091@gmail.com>
2023-05-18 21:05:47 +0300

    v1.4

--- store/models.py
+++ store/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 
@@ -8,3 +9,22 @@ class Book(models.Model):
 
     def __str__(self):
         return f'{self.name}, {self.price}'
+
+
+class UserBookRelation(models.Model):
+    RATE_CHOICES = (
+        (1, 'Очень плохо'),
+        (2, 'Плохо'),
+        (3, 'Нормально'),
+        (4, 'Хорошо'),
+        (5, 'Очень хорошо'),
+    )
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    like = models.BooleanField(default=False)
+    in_bookmarks = models.BooleanField(default=False)
+    rate = models.PositiveSmallIntegerField(choices=RATE_CHOICES)
+
+    def __str__(self):
+        return f'{self.user}, {self.book}'
